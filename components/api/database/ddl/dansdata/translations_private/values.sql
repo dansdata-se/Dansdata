87a7de383ad419b0789b6b49a3f466549c58e822
Felix Zedén Yverås <felix@fzy.se>
2024-11-30 13:28:48 +0100

    style(database): reformat sql files
    Looks like datagrip did not pick up the 2 space indentation specified in
    editorconfig.

--- components/api/database/ddl/dansdata/translations_private/values.sql
+++ components/api/database/ddl/dansdata/translations_private/values.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS translations_private.values (
-    metadata_id INTEGER NOT NULL
-        CONSTRAINT values_metadata_id_metadatas_id_fk
-            REFERENCES translations_private.metadatas
-            ON UPDATE CASCADE
-            ON DELETE CASCADE,
-    language_id INTEGER NOT NULL
-        CONSTRAINT values_language_id_languages_id_fk
-            REFERENCES translations_private.languages
-            ON UPDATE CASCADE
-            ON DELETE CASCADE,
-    value TEXT NOT NULL,
-    CONSTRAINT values_pk
-        PRIMARY KEY (language_id, metadata_id)
+  metadata_id INTEGER NOT NULL
+    CONSTRAINT values_metadata_id_metadatas_id_fk
+      REFERENCES translations_private.metadatas
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  language_id INTEGER NOT NULL
+    CONSTRAINT values_language_id_languages_id_fk
+      REFERENCES translations_private.languages
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  value TEXT NOT NULL,
+  CONSTRAINT values_pk
+    PRIMARY KEY (language_id, metadata_id)
 );
 
 COMMENT ON TABLE translations_private.values IS 'Contains the translated texts for translations_private.';
@@ -21,4 +21,4 @@ COMMENT ON COLUMN translations_private.values.language_id IS 'Reference to the l
 COMMENT ON COLUMN translations_private.values.value IS 'The translated text.';
 
 ALTER TABLE translations_private.values
-    OWNER TO translations_owner;
+  OWNER TO translations_owner;
--- components/api/database/ddl/dansdata/translations_public/translations.sql
+++ components/api/database/ddl/dansdata/translations_public/translations.sql
@@ -1,17 +1,17 @@
 CREATE OR REPLACE VIEW translations_public.translations(id, language_code, value) AS
-    SELECT
-        m.external_id AS id,
-        l.code AS language_code,
-        COALESCE(m.override_value, v.value, ''::TEXT) AS value
-        FROM
-            translations_private.metadatas m
-                CROSS JOIN translations_private.languages l
-                LEFT JOIN translations_private.values v
-                    ON v.metadata_id = m.id AND v.language_id = l.id;
+  SELECT
+    m.external_id AS id,
+    l.code AS language_code,
+    COALESCE(m.override_value, v.value, ''::TEXT) AS value
+    FROM
+      translations_private.metadatas m
+        CROSS JOIN translations_private.languages l
+        LEFT JOIN translations_private.values v
+        ON v.metadata_id = m.id AND v.language_id = l.id;
 
 COMMENT ON VIEW translations_public.translations IS 'A view of all translated strings, with values for all languages.';
 
 ALTER TABLE translations_public.translations
-    OWNER TO translations_owner;
+  OWNER TO translations_owner;
 
 GRANT SELECT ON translations_public.translations TO PUBLIC;
