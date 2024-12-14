cb4d5aa3e779ea9cfaea1176373d3cebe1525ca1
Felix Zedén Yverås <felix@fzy.se>
2024-12-14 15:58:18 +0100

    feat(shuffle): configure logging and opentelemetry
    Opentelemetry will need further config as it currently is completely
    untested. It also complains that export failed as I have not set up a
    local receiver for this data yet.
    
    The implementation is highly based on
    https://github.com/tokio-rs/tracing-opentelemetry/blob/v0.1.x/examples/opentelemetry-otlp.rs#L10

---
+++ components/shuffle/src/logging.rs
@@ -0,0 +1,104 @@
+use crate::built_info;
+use opentelemetry::trace::TracerProvider as _;
+use opentelemetry::{global, KeyValue};
+use opentelemetry_sdk::trace::TracerProvider;
+use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::{
+    metrics::{MeterProviderBuilder, PeriodicReader, SdkMeterProvider},
+    runtime::Tokio,
+};
+use opentelemetry_semantic_conventions::{attribute, SCHEMA_URL};
+use tracing::Level;
+use tracing_opentelemetry::{MetricsLayer, OpenTelemetryLayer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+pub struct LoggingGuard {
+    tracer_provider: TracerProvider,
+    meter_provider: SdkMeterProvider,
+}
+
+impl Drop for LoggingGuard {
+    fn drop(&mut self) {
+        if let Err(err) = self.tracer_provider.shutdown() {
+            eprintln!("{err:?}");
+        }
+        if let Err(err) = self.meter_provider.shutdown() {
+            eprintln!("{err:?}");
+        }
+    }
+}
+
+pub fn init_logging() -> LoggingGuard {
+    let tracer_provider = init_tracer_provider();
+    let meter_provider = init_meter_provider();
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::filter::LevelFilter::from_level(
+            Level::DEBUG,
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .with(MetricsLayer::new(meter_provider.clone()))
+        .with(OpenTelemetryLayer::new(
+            tracer_provider.tracer(built_info::PKG_NAME),
+        ))
+        .init();
+
+    LoggingGuard {
+        tracer_provider,
+        meter_provider,
+    }
+}
+
+fn init_tracer_provider() -> TracerProvider {
+    let exporter = opentelemetry_otlp::SpanExporter::builder()
+        .with_tonic()
+        .build()
+        .unwrap();
+
+    TracerProvider::builder()
+        .with_batch_exporter(exporter, Tokio)
+        .build()
+}
+
+fn init_meter_provider() -> SdkMeterProvider {
+    let exporter = opentelemetry_otlp::MetricExporter::builder()
+        .with_tonic()
+        .with_temporality(opentelemetry_sdk::metrics::Temporality::default())
+        .build()
+        .unwrap();
+
+    let reader = PeriodicReader::builder(exporter, Tokio)
+        .with_interval(std::time::Duration::from_secs(30))
+        .build();
+
+    // For local development debugging
+    let stdout_reader =
+        PeriodicReader::builder(opentelemetry_stdout::MetricExporter::default(), Tokio).build();
+
+    let meter_provider = MeterProviderBuilder::default()
+        .with_resource(resource())
+        .with_reader(reader)
+        .with_reader(stdout_reader)
+        .build();
+    global::set_meter_provider(meter_provider.clone());
+
+    meter_provider
+}
+
+fn resource() -> Resource {
+    Resource::from_schema_url(
+        [
+            KeyValue::new(attribute::SERVICE_NAME, built_info::PKG_NAME),
+            KeyValue::new(
+                attribute::SERVICE_VERSION,
+                format!(
+                    "{} ({})",
+                    built_info::PKG_VERSION,
+                    built_info::GIT_COMMIT_HASH_SHORT.expect("Failed to get git commit hash")
+                ),
+            ),
+        ],
+        SCHEMA_URL,
+    )
+}
--- components/shuffle/src/main.rs
+++ components/shuffle/src/main.rs
@@ -1,13 +1,46 @@
+mod built_info;
+mod logging;
+
+use crate::logging::init_logging;
 use axum::routing::get;
 use axum::{Json, Router};
 use serde_json::json;
+use tower::ServiceBuilder;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tower_http::LatencyUnit;
+use tracing::{info, Level};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route(
-        "/",
-        get(|| async { Json(json!({"message": "Hello, World!"})) }),
+    let _logging_guard = init_logging();
+
+    info!(
+        "Starting {} v{} ({}) built with {} at {} for {}",
+        built_info::PKG_NAME,
+        built_info::PKG_VERSION,
+        built_info::GIT_COMMIT_HASH_SHORT.expect("Failed to get git commit hash"),
+        built_info::RUSTC_VERSION,
+        built::util::strptime(built_info::BUILT_TIME_UTC).to_rfc3339(),
+        built_info::TARGET,
     );
+
+    let app = Router::new()
+        .route(
+            "/",
+            get(|| async { Json(json!({"message": "Hello, World!"})) }),
+        )
+        .layer(
+            ServiceBuilder::new().layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(DefaultMakeSpan::new().include_headers(true))
+                    .on_request(DefaultOnRequest::new().level(Level::INFO))
+                    .on_response(
+                        DefaultOnResponse::new()
+                            .level(Level::INFO)
+                            .latency_unit(LatencyUnit::Micros),
+                    ),
+            ),
+        );
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
